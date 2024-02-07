dc1187fd8f95b6be067f93c48b5255ca44309907
mpayne30 <Mpayne30@murraystate.edu>
2024-02-06 18:26:47 -0600

    Deleted landing_page route
    Deleted landing page route to create landing page on main page.tsx

--- app/landing_page/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Landing_Page() {
-    return (
-        <main>
-        <p>Landing page, may be removed and make default page landing LandingPage</p>
-        </main>
-    );
-  }
