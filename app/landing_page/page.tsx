54baa13063b72e2cd3f1d224b3bc3c14979a9791
mpayne30 <Mpayne30@murraystate.edu>
2024-02-06 20:42:37 -0600

    Almost working temp landing page with nav bar

--- app/landing_page/page.tsx
+++ app/landing_page/page.tsx
@@ -1,19 +1,10 @@
+import NavBar from "./navbar";
+
 export default function Landing_Page()  {
+
     return (
     <header className="dark:bg-dark-bckground sticky top-0 z-[20] mx-auto flex items-center justify-between">
-      <nav>
-        <ul>
-          <li>
-              <a href="about">About</a>
-          </li>
-          <li>
-              <a href="login">Login</a>
-          </li>
-          <li>
-              <a href="support" >Support</a>
-          </li>
-        </ul>
-      </nav>
+        <NavBar />
     </header>
   );
 };
